6714db2e0fa172b872dc8bc7633d96e66be54b60
Kyung-Hyun Na <intnakh@gmail.com>
2013-06-03 11:13:34 -0400

    HEROKU add

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var express = require('express')
 var app = express();
 
 // all environments
+//app.set('host', process.env.IP);
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
